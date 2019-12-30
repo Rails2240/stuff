27cb0a435a7019cb73cf39bc3f376a7c033483f4
Chris <rails2240@gmail.com>
2019-12-30 14:47:33 -0500

    testing stuff

---
+++ cal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "Hello World!! Please enter a number to dubble:" "\n";
+
+int A,B,C;
+
+
+cin >> A;
+
+B=2;
+
+C=A*B;
+
+
+cout << C;
+
+cout << "\n";
+
+
+return 0;
+
+}
